e2f1c40ff51881ad08c7fbd9387606990429fcd4
pepperpo <beppelisi@gmail.com>
2020-04-22 11:42:55 +0100

    Fixed data download
    I still need to add the date and time control.
    From now I start working on adding traces.

--- dash_app/app.py
+++ dash_app/app.py
@@ -7,7 +7,7 @@ from dash.dependencies import Input, Output, State
 from config import config_app
 from layout import app_layout, make_header, make_main,make_footer
 #from plots import bar_plot, scatter_plot, cnt_plot
-from functions import generate_plot,load_data
+from functions import generate_plot,load_data,data2dropdown
 import json
 
 import sys
@@ -16,7 +16,7 @@ import sys
 # logger.addHandler(logging.StreamHandler(stream=sys.stderr))
 # logger.setLevel(logging.DEBUG)
 
-debug_flag = False
+debug_flag = True
 
 server = Flask(__name__)
 
@@ -32,6 +32,7 @@ app = config_app(app, debug=debug_flag)
 # Content of each div is a function input
 app.layout = app_layout(header=make_header(),main=make_main(),footer=make_footer())
 
+all_data_dict = {}
 
 @app.callback(Output('page-main', 'children'), [Input('url', 'pathname')])
 def routing(pathname):
@@ -70,17 +71,20 @@ def routing(pathname):
                Input('columns_dropdown', 'value'),
                Input('norm_dropdown', 'value'),
                Input('log_radio', 'value'),
-               Input('dash-session', 'data'),
                Input('region_dropdown', 'value'),
-               Input('province_dropdown', 'value')
-               ])
-def update_plot(aggr_in,col_in,norm_in,log_in,dash_session,region_in,prov_in):
-    rv = generate_plot(dash_session,aggr_in,col_in,norm_in,log_in,region_in,prov_in)
+               Input('province_dropdown', 'value'),
+               Input('loading-submit-cnt2', 'children')],
+              [State('country_dropdown','value')])
+def update_plot(aggr_in, col_in, norm_in, log_in, region_in, prov_in, update_done, country_in):
+    print(aggr_in, col_in, norm_in, log_in, region_in, prov_in, update_done)
+    if not update_done:
+        raise PreventUpdate
+
+    rv = generate_plot(all_data_dict[country_in],aggr_in,col_in,norm_in,log_in,region_in,prov_in)
     return rv,'done'
 
 @app.callback([Output('region_dropdown', 'options'),
                Output('province_dropdown','options'),
-               Output('dash-session', 'data'),
                Output('loading-submit-cnt2','children')],
                [Input('country_dropdown', 'value')])
 def load_data_callback(country_in):
@@ -88,12 +92,9 @@ def load_data_callback(country_in):
     if not country_in:
         raise PreventUpdate
 
-    dash_session,region_list,province_list = load_data(country_in)
-
-    region_drp = [{'label': cur_opt, 'value': cur_opt} for cur_opt in region_list]
-    province_drp = [{'label': cur_opt, 'value': cur_opt} for cur_opt in province_list]
-
-    return region_drp,province_drp,dash_session,'done'
+    load_data(country_in,all_data_dict)
+    region_drp, province_drp = data2dropdown(country_in, all_data_dict)
+    return region_drp, province_drp, 'done'
 
 
 
@@ -104,35 +105,38 @@ def load_data_callback(country_in):
                Output('norm_dropdown', 'value'),
                ],
                [Input('region_dropdown', 'value'),
-                Input('province_dropdown', 'value'),
-                Input('dash-session', 'data')],
+                Input('province_dropdown', 'value')],
               [State('columns_dropdown', 'value'),
-               State('norm_dropdown', 'value')]
+               State('norm_dropdown', 'value'),
+               State('country_dropdown','value')]
               )
-def update_col_options(region_in,province_in,dash_session,col_st,norm_st):
+def update_col_options(region_in,province_in,col_st,norm_st,country_in):
+
+    if not (region_in or province_in):
+        raise PreventUpdate
+
     col_drp = []
     norm_drp = []
     new_col_val = None
     new_norm_val = None
 
-    if dash_session:
-        dash_session = json.loads(dash_session)
+    cur_dict = all_data_dict[country_in]
 
-        if province_in and not region_in:
-            options = dash_session['options_province']
-            col_drp = [{'label': cur_opt, 'value': cur_opt} for cur_opt in options]
-        else:
-            options = dash_session['options_nation']
-            col_drp = [{'label': cur_opt, 'value': cur_opt} for cur_opt in options]
-            norm_drp = [{'label': cur_opt, 'value': cur_opt} for cur_opt in options]
+    if province_in and not region_in:
+        options = cur_dict['options_province']
+        col_drp = [{'label': cur_opt, 'value': cur_opt} for cur_opt in options]
+    else:
+        options = cur_dict['options_nation']
+        col_drp = [{'label': cur_opt, 'value': cur_opt} for cur_opt in options]
+        norm_drp = [{'label': cur_opt, 'value': cur_opt} for cur_opt in options]
 
-        if province_in:
-            norm_drp = []
+    if province_in:
+        norm_drp = []
 
-        if col_st:
-            new_col_val = [value for value in options if value in col_st]
-        if norm_st:
-            new_norm_val = [value for value in options if value in norm_st]
+    if col_st:
+        new_col_val = [value for value in options if value in col_st]
+    if norm_st:
+        new_norm_val = [value for value in options if value in norm_st]
 
     return col_drp,norm_drp,'done',new_col_val,new_norm_val
 
--- dash_app/functions.py
+++ dash_app/functions.py
@@ -2,65 +2,65 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import json
+import time
 
 def generate_plot(dash_session,aggr_in,col_in,norm_in,log_in,region_in,prov_in):
 
     fig = go.Figure()
     fig.update_layout( margin={'t': 0})
 
-    if dash_session:
+    options = dash_session['options_nation']
+    if not col_in:
+        col_in = options
 
-        dash_session = json.loads(dash_session)
+    fig_data = []
+    annot_flag = [False]
 
-        options = dash_session['options_nation']
-        if not col_in:
-            col_in = options
+    t1 = time.time()
 
-        fig_data = []
-        annot_flag = [False]
+    if not region_in and not prov_in:
+        df = pd.DataFrame(dash_session['nation'])
+        plot_df(df, fig_data,annot_flag,aggr_in,col_in,norm_in,log_in)
 
-        if not region_in and not prov_in:
-            df = pd.DataFrame(dash_session['nation'])
-            plot_df(df, fig_data,annot_flag,aggr_in,col_in,norm_in,log_in)
-
-        if region_in:
-            for cur_region in region_in:
-                if cur_region == '{}[all regions]'.format(dash_session['nation_str']):
-                    df = pd.DataFrame(dash_session['nation'])
-                else:
-                    df = pd.DataFrame(dash_session['region'])
-                    cur_idx = df['denominazione_regione']==cur_region
-                    df = df.loc[cur_idx]
-                plot_df(df, fig_data,annot_flag,aggr_in,col_in,norm_in,log_in,suffix='({})'.format(cur_region))
-
-        if prov_in:
-            for cur_prov in prov_in:
-                df = pd.DataFrame(dash_session['province'])
-                cur_idx = df['denominazione_provincia'] == cur_prov
+    if region_in:
+        for cur_region in region_in:
+            if cur_region == '{}[all regions]'.format(dash_session['nation_str']):
+                df = pd.DataFrame(dash_session['nation'])
+            else:
+                df = pd.DataFrame(dash_session['region'])
+                cur_idx = df['denominazione_regione']==cur_region
                 df = df.loc[cur_idx]
-                cur_col = [value for value in col_in if value in dash_session['options_province']]
-                plot_df(df, fig_data, annot_flag, aggr_in, cur_col, norm_in, log_in, suffix='({})'.format(cur_prov))
+            plot_df(df, fig_data,annot_flag,aggr_in,col_in,norm_in,log_in,suffix='({})'.format(cur_region))
+
+    if prov_in:
+        for cur_prov in prov_in:
+            df = pd.DataFrame(dash_session['province'])
+            cur_idx = df['denominazione_provincia'] == cur_prov
+            df = df.loc[cur_idx]
+            cur_col = [value for value in col_in if value in dash_session['options_province']]
+            plot_df(df, fig_data, annot_flag, aggr_in, cur_col, norm_in, log_in, suffix='({})'.format(cur_prov))
+
+    print('Time elapsed {}'.format(time.time()-t1))
 
 
+    if annot_flag[0] is True:
+        annotations = [
+            dict(
+                x=0.5,
+                y=0.1,
+                showarrow=False,
+                text="Some variables were deleted because log of a negative value",
+                xref="paper",
+                yref="paper"
+            )]
+    else:
+        annotations = []
 
-        if annot_flag[0] is True:
-            annotations = [
-                dict(
-                    x=0.5,
-                    y=0.1,
-                    showarrow=False,
-                    text="Some variables were deleted because log of a negative value",
-                    xref="paper",
-                    yref="paper"
-                )]
-        else:
-            annotations = []
+    fig = go.Figure(data=fig_data)
 
-        fig = go.Figure(data=fig_data)
-
-        # Change the bar mode
-        #fig.update_layout(barmode='relative',hovermode='x unified')
-        fig.update_layout(hovermode='x unified',yaxis_type=log_in,annotations=annotations,margin={'t': 0})
+    # Change the bar mode
+    #fig.update_layout(barmode='relative',hovermode='x unified')
+    fig.update_layout(hovermode='x unified',yaxis_type=log_in,annotations=annotations,margin={'t': 0})
 
     return fig
 
@@ -94,54 +94,65 @@ def plot_df(df,fig_data,annot_flag,aggr_in,col_in,norm_in,log_in,suffix=''):
 
 
 
-def load_data(country_in):
+def load_data(country_in,all_data_dict):
 
     from constants import translation_dict
 
+    if not all_data_dict:
+        if country_in == 'Italy':
+
+            print('downloading')
+
+            cur_tr = translation_dict[country_in]
+
+            df_nation = pd.read_csv(
+                'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv',
+                header=0)
+            df_region = pd.read_csv(
+                'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv',
+                header=0)
+            df_province = pd.read_csv(
+                'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province.csv',
+                header=0)
+
+            options_nation_region_it = ['ricoverati_con_sintomi','terapia_intensiva','totale_ospedalizzati','isolamento_domiciliare','totale_positivi','dimessi_guariti','deceduti','totale_casi','tamponi','casi_testati']
+            options_province_it = ['totale_casi']
+
+            #apply translations
+            options_nation_region = [cur_tr[cur_var] for cur_var in options_nation_region_it]
+            options_province = [cur_tr[cur_var] for cur_var in options_province_it]
+            df_nation.rename(columns=cur_tr,inplace=True)
+            df_region.rename(columns=cur_tr,inplace=True)
+            df_province.rename(columns=cur_tr,inplace=True)
+
+            data = {'nation':df_nation,
+                    'options_nation':options_nation_region,
+                    'region': df_region,
+                    'options_region': options_nation_region,
+                    'province': df_province,
+                    'options_province': options_province,
+                    'nation_str': country_in,
+                    }
+
+            all_data_dict[country_in] = data
+
+
+
+
+def data2dropdown(country_in,dict_in):
     region_list = []
     province_list = []
-
-    if country_in == 'Italy':
-
-        cur_tr = translation_dict[country_in]
-
-        df_nation = pd.read_csv(
-            'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv',
-            header=0)
-        df_region = pd.read_csv(
-            'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv',
-            header=0)
-        df_province = pd.read_csv(
-            'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province.csv',
-            header=0)
-
-        options_nation_region_it = ['ricoverati_con_sintomi','terapia_intensiva','totale_ospedalizzati','isolamento_domiciliare','totale_positivi','dimessi_guariti','deceduti','totale_casi','tamponi','casi_testati']
-        options_province_it = ['totale_casi']
-
-        #apply translations
-        options_nation_region = [cur_tr[cur_var] for cur_var in options_nation_region_it]
-        options_province = [cur_tr[cur_var] for cur_var in options_province_it]
-        df_nation.rename(columns=cur_tr,inplace=True)
-        df_region.rename(columns=cur_tr,inplace=True)
-        df_province.rename(columns=cur_tr,inplace=True)
-
-        data = {'nation':df_nation.to_dict(),
-                'options_nation':options_nation_region,
-                'region': df_region.to_dict(),
-                'options_region': options_nation_region,
-                'province': df_province.to_dict(),
-                'options_province': options_province,
-                'nation_str': country_in,
-                }
-
-        region_list = ['{}[all regions]'.format(country_in)] + sorted(df_region['denominazione_regione'].unique().tolist())
-        province_list = sorted(df_province['denominazione_provincia'].unique().tolist())
-
+    cur_dict = dict_in[country_in]
+    if country_in=='Italy':
+        region_list = ['{}[all regions]'.format(country_in)] + sorted(cur_dict['region']['denominazione_regione'].unique().tolist())
+        province_list = sorted(cur_dict['province']['denominazione_provincia'].unique().tolist())
         province_list.remove('In fase di definizione/aggiornamento')
 
-    dash_session = json.dumps(data)
+        region_drp = [{'label': cur_opt, 'value': cur_opt} for cur_opt in region_list]
+        province_drp = [{'label': cur_opt, 'value': cur_opt} for cur_opt in province_list]
 
-    return dash_session,region_list,province_list
+    return region_drp, province_drp
+
 
 
 if __name__ == "__main__":
